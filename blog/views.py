0f508210b5eaf48e5c5aa131b02b9e695e42d4c5
Pawel Wroblewski <pawelwroblewski@rocketmail.com>
2019-08-06 18:33:54 +0200

    databases and migrations handling

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {'author': 'jan',
-     'title': 'title1',
-     'content': 'first content',
-     'date_posted': 'date1'},
-    {'author': 'paul',
-     'title': 'title2',
-     'content': 'second content',
-     'date_posted': 'date2'},
-
-
-]
+from .models import Post
 
 
 def home(request):
     # return HttpResponse('<h1> Blog home </h1>')
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
 
     return render(request, 'blog/home.html', context)
 
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -86,18 +86,19 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # commented out as those lines prevented entering easy passwords for new users
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
