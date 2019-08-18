01ce5366e3c3ead2dde8b20316c4a08f537c100f
Pawel Wroblewski <pawelwroblewski@rocketmail.com>
2019-08-18 16:55:49 +0200

    User registration form + backend

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
