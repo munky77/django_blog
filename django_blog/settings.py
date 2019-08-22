be88d60ff575a09d1141a20260546163ecb4f4cf
Pawel Wroblewski <pawelwroblewski@rocketmail.com>
2019-08-22 15:27:37 +0200

    profile pic and profile update form

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -127,3 +127,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# profile pics related
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save()
+        img_ = Image.open(self.img.path)
+        if img_.height > 300 or img_.width > 300:
+            output_size = (300, 300)
+            img_.thumbnail(output_size)
+            img_.save(self.img.path)
